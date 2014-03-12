e85b205b84ce97bfa4daec9f26852936cc9114d4
jamesrossiter <james@jamesrossiter.co.uk>
2014-03-12 11:14:00 +0000

    enforce use of dd-MM-yyyy in RadarDateConverter (used by all RadarRequiredDateTextField)

--- patientview-parent/radar/src/main/java/org/patientview/radar/util/RadarDateConverter.java
+++ patientview-parent/radar/src/main/java/org/patientview/radar/util/RadarDateConverter.java
@@ -3,7 +3,7 @@ package org.patientview.radar.util;
 import org.apache.wicket.util.convert.converter.DateConverter;
 import org.apache.wicket.util.string.Strings;
 import org.patientview.util.CommonUtils;
-
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
@@ -13,6 +13,12 @@ import java.util.Locale;
 public class RadarDateConverter extends DateConverter {
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Enforce strict date parsing using CommonUtils
+     * @param value Date string to convert to Date object
+     * @param locale
+     * @return Output of CommonUtils.parseUKDate(value);
+     */
     @Override
     public Date convertToObject(final String value, final Locale locale) {
         if ((value == null) || Strings.isEmpty(value)) {
@@ -20,5 +26,16 @@ public class RadarDateConverter extends DateConverter {
         } else {
             return CommonUtils.parseUKDate(value);
         }
+    }
+
+    /**
+     * Enforce standard conversion of dates to RADAR dd-MM-yyyy format (not default en_US format of M/d/yy)
+     * @param value
+     * @param locale
+     * @return
+     */
+    @Override
+    public String convertToString(final Date value, final Locale locale) {
+        return new SimpleDateFormat("dd-MM-yyyy").format(value);
     }
 }
