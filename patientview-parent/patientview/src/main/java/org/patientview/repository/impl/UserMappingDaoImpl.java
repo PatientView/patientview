043e7a0839bb40f6ce57a70b4a34109d0ba47112
jamesrossiter <james@jamesrossiter.co.uk>
2014-03-17 14:52:46 +0000

    changed to use usermapping table during import, checking existing code. NOTE: a lot of references to centres, seem to be analogous to unit (unit/centre code) but really needs refactoring out of data model.

--- patientview-parent/patientview/src/main/java/org/patientview/repository/impl/UserMappingDaoImpl.java
+++ patientview-parent/patientview/src/main/java/org/patientview/repository/impl/UserMappingDaoImpl.java
@@ -155,6 +155,21 @@ public class UserMappingDaoImpl extends AbstractHibernateDAO<UserMapping> implem
     }
 
     @Override
+    public List<UserMapping> getAllForNhsNo(String nhsNo, String unitCode) {
+
+        CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<UserMapping> criteria = builder.createQuery(UserMapping.class);
+        Root<UserMapping> from = criteria.from(UserMapping.class);
+        List<Predicate> wherePredicates = new ArrayList<Predicate>();
+
+        wherePredicates.add(builder.equal(from.get(UserMapping_.nhsno), nhsNo));
+        wherePredicates.add(builder.equal(from.get(UserMapping_.unitcode), unitCode));
+
+        buildWhereClause(criteria, wherePredicates);
+        return getEntityManager().createQuery(criteria).getResultList();
+    }
+
+    @Override
     public List<UserMapping> getAllForNhsNo(String nhsNo, Specialty specialty) {
 
         CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
