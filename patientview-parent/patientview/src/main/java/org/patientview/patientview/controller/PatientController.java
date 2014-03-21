bcc3fd6fcb61d1ce6482f93c7612f5201ec62b88
James Eaton <james@solidstategroup.com>
2014-03-21 15:10:20 +0000

    Trello 443 - Added extra fields

---
+++ patientview-parent/patientview/src/main/java/org/patientview/patientview/controller/PatientController.java
@@ -0,0 +1,28 @@
+package org.patientview.patientview.controller;
+
+import org.patientview.ibd.action.BaseAction;
+import org.patientview.patientview.controller.form.PatientInput;
+import org.patientview.service.UserManager;
+
+import javax.inject.Inject;
+
+/**
+ * Created by james@solidstategroup.com on 21/03/2014.
+ */
+public class PatientController extends BaseAction {
+
+
+    @Inject
+    private UserManager userManager;
+
+    public void addPatient(PatientInput patientForm) {
+
+
+    }
+
+    public void updatePatient(PatientInput patientInput) {
+
+
+    }
+
+}
---
+++ patientview-parent/patientview/src/main/java/org/patientview/patientview/controller/form/IbdPatientInput.java
@@ -0,0 +1,14 @@
+package org.patientview.patientview.controller.form;
+
+/**
+ * Created by eatek on 21/03/2014.
+ */
+public class IbdPatientInput {
+
+    private String primaryDiagnosis;
+    private String diseaseExtent;
+    private Integer yearOfDiagnosis;
+    private String familyHistory;
+
+
+}
