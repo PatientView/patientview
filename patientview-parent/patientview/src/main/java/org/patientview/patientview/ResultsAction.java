8edafcb57dfeb12b06afc3ef213eb8af0331717e
jamesrossiter <james@jamesrossiter.co.uk>
2014-04-04 15:06:17 +0100

    RPV-363 - Error in Log exceptions to logger.trace

--- patientview-parent/patientview/src/main/java/org/patientview/patientview/ResultsAction.java
+++ patientview-parent/patientview/src/main/java/org/patientview/patientview/ResultsAction.java
@@ -172,7 +172,9 @@ public class ResultsAction extends ActionSupport {
                     dataMinValue = Double.parseDouble(resultValue1);
                 }
             } catch (NumberFormatException e) {
+                LOGGER.trace("NumberFormatException: " + e.toString());
             } catch (NullPointerException e) {
+                LOGGER.trace("NullPointerException: " + e.toString());
             }
 
             try {
@@ -180,7 +182,9 @@ public class ResultsAction extends ActionSupport {
                     dataMaxValue = Double.parseDouble(resultValue1);
                 }
             } catch (NumberFormatException e) {
+                LOGGER.trace("NumberFormatException: " + e.toString());
             } catch (NullPointerException e) {
+                LOGGER.trace("NullPointerException: " + e.toString());
             }
 
             sb.append("{\"c\":[");
