059d723f739ad3d6bad626f6fde3a3fd75d559d1
jamesrossiter <james@jamesrossiter.co.uk>
2014-04-23 15:42:59 +0100

    Card #204 - patient info links patient info now available to set on radar group creation

--- patientview-parent/patientview/src/main/java/org/patientview/patientview/unit/UnitAddAction.java
+++ patientview-parent/patientview/src/main/java/org/patientview/patientview/unit/UnitAddAction.java
@@ -23,46 +23,59 @@
 
 package org.patientview.patientview.unit;
 
-import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.patientview.model.Unit;
+import org.patientview.patientview.edtacode.EdtaCodeUtils;
 import org.patientview.patientview.logon.LogonUtils;
-import org.patientview.utils.LegacySpringUtils;
+import org.patientview.patientview.model.EdtaCode;
+import org.patientview.service.EdtaCodeManager;
+import org.patientview.service.UnitManager;
+import org.springframework.web.struts.ActionSupport;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-public class UnitAddAction extends Action {
+public class UnitAddAction extends ActionSupport {
+
+    private UnitManager unitManager;
+    private EdtaCodeManager edtaCodeManager;
 
     public ActionForward execute(
         ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
             throws Exception {
+
+        unitManager = getWebApplicationContext().getBean(UnitManager.class);
+        edtaCodeManager = getWebApplicationContext().getBean(EdtaCodeManager.class);
 
         Unit unit = new Unit();
         UnitUtils.buildUnit(unit, form);
         //a new unitcode doesn't exist,
         // so any user doesn't have SecurityConfig.UNIT_ACCESS permission to check this code
         // Unit existedUnit = LegacySpringUtils.getUnitManager().get(unit.getUnitcode());
-        boolean duplicate = LegacySpringUtils.getUnitManager().checkDuplicateUnitCode(unit.getUnitcode());
+        boolean duplicate = unitManager.checkDuplicateUnitCode(unit.getUnitcode());
         if (duplicate) {
             request.setAttribute("UnitExisted", true);
             return mapping.findForward("input");
         }
-        LegacySpringUtils.getUnitManager().save(unit);
+        unitManager.save(unit);
 
         boolean isRadarGroup = "radargroup".equalsIgnoreCase(mapping.getParameter());
 
-        List items = LegacySpringUtils.getUnitManager().getAdminsUnits(isRadarGroup);
+        List items = unitManager.getAdminsUnits(isRadarGroup);
         request.setAttribute("units", items);
 
         if (isRadarGroup) {
+            // if radar group, save patient info links in 1:1 EdtaCode object
+            EdtaCode edtaCode = new EdtaCode(unit.getUnitcode());
+            edtaCode.setLinkType("unitLinks");
+            EdtaCodeUtils.build(request, edtaCode);
+            edtaCodeManager.save(edtaCode);
             request.setAttribute("isRadarGroup", isRadarGroup);
         }
 
         return LogonUtils.logonChecks(mapping, request);
     }
-
 }
