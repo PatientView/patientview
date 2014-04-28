93de1632fa108bd4d8f67214af37992ef57c38db
jamesrossiter <james@jamesrossiter.co.uk>
2014-04-28 10:14:23 +0100

    RPV-410  Messaging Validation added required data fix for dev and future live branches to set conversation.type to MESSAGE where is NULL

--- patientview-parent/common/src/main/resources/sql/features/30/30.sql
+++ patientview-parent/common/src/main/resources/sql/features/30/30.sql
@@ -16,3 +16,5 @@ CREATE TABLE `conversation_status` (
 INSERT INTO conversation_status (id, status, closedStatus) VALUES (1, 'Open - Ongoing', 0), (2, 'Closed - Amended', 1), (3, 'Closed - Explained Records', 1), (4, 'Closed - Will Discuss in Clinic', 1);
 
 ALTER TABLE user ADD COLUMN `feedbackRecipient` tinyint(1) NOT NULL DEFAULT '0';
+
+UPDATE conversation SET type='MESSAGE' WHERE type IS NULL;
