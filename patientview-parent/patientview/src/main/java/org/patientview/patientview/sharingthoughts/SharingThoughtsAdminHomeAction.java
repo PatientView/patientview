8f4e720945ea97fa17589760c87f0ddd15c33224
jamesrossiter <james@jamesrossiter.co.uk>
2014-04-29 12:27:10 +0100

    Sharing Thoughts superadmin sees all sharing thoughts, other users just see those they are responders for (or in their unit if user.sharedThoughtAdministrator = true)

--- patientview-parent/patientview/src/main/java/org/patientview/patientview/sharingthoughts/SharingThoughtsAdminHomeAction.java
+++ patientview-parent/patientview/src/main/java/org/patientview/patientview/sharingthoughts/SharingThoughtsAdminHomeAction.java
@@ -3,8 +3,10 @@ package org.patientview.patientview.sharingthoughts;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.patientview.patientview.model.User;
 import org.patientview.service.SecurityUserManager;
 import org.patientview.service.SharedThoughtManager;
+import org.patientview.service.UserManager;
 import org.springframework.web.struts.ActionSupport;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +15,7 @@ public class SharingThoughtsAdminHomeAction extends ActionSupport {
 
     private SharedThoughtManager sharedThoughtManager;
     private SecurityUserManager securityUserManager;
+    private UserManager userManager;
 
     public ActionForward execute(
             ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
@@ -20,9 +23,15 @@ public class SharingThoughtsAdminHomeAction extends ActionSupport {
 
         sharedThoughtManager = getWebApplicationContext().getBean(SharedThoughtManager.class);
         securityUserManager = getWebApplicationContext().getBean(SecurityUserManager.class);
+        userManager = getWebApplicationContext().getBean(UserManager.class);
+        User loggedInUser = securityUserManager.getLoggedInUser();
 
-        request.setAttribute(
-                "sharedThoughts", sharedThoughtManager.getStaffThoughtList(securityUserManager.getLoggedInUser()));
+        if (userManager.getCurrentSpecialtyRole(loggedInUser).equalsIgnoreCase("superadmin")) {
+            request.setAttribute("sharedThoughts", sharedThoughtManager.getAll());
+        } else {
+            request.setAttribute("sharedThoughts", sharedThoughtManager.getStaffThoughtList(loggedInUser));
+        }
+
 
         return mapping.findForward("success");
     }
